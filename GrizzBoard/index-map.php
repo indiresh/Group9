7529c465ad9b5fea2a1337bb2401f4a910a86635
Seth Jones <seth.jones@nerdheroes.com>
2012-11-25 13:02:44 -0500

    Initial desing and css start

---
+++ GrizzBoard/index-map.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Grizzboard - Ver .01</title>
+<link href="css/main.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="top-bar"> </div>
+<div class="header-wrapper">
+  <div class="header"> </div>
+  <div id="login">Login</div>
+</div>
+<div class="main-wrapper">
+  <div class="content-wrapper"><?php include "map.php";?> </div>
+</div>
+</body>
+</html>
---
+++ GrizzBoard/map.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+</head>
+
+<body>
+
+<div id="container">
+<h2> Locations of Bulletin Boards Throughout OU's Campus </h2>
+<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=211058690808209926622.0004ce68ad4c5d97dafe1&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=42.674343,-83.215277&amp;spn=0.007573,0.013733&amp;z=16&amp;output=embed"></iframe><br /><small>View <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=211058690808209926622.0004ce68ad4c5d97dafe1&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=42.674343,-83.215277&amp;spn=0.007573,0.013733&amp;z=16&amp;source=embed" style="color:#0000FF;text-align:left">OU Campus</a> in a larger map</small>
+</body>
+</html>
