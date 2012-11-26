8a23ecf044a7c8c57292c078652b32c4524eca31
Ryan D <rmdwyer@oakland.edu>
2012-11-25 19:32:49 -0500

    edited map file

--- GrizzBoard/map.php
+++ GrizzBoard/map.php
@@ -8,8 +8,28 @@
 
 <body>
 
-<div id="container">
 <h2> Locations of Bulletin Boards Throughout OU's Campus </h2>
-<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=211058690808209926622.0004ce68ad4c5d97dafe1&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=42.674343,-83.215277&amp;spn=0.007573,0.013733&amp;z=16&amp;output=embed"></iframe><br /><small>View <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=211058690808209926622.0004ce68ad4c5d97dafe1&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=42.674343,-83.215277&amp;spn=0.007573,0.013733&amp;z=16&amp;source=embed" style="color:#0000FF;text-align:left">OU Campus</a> in a larger map</small>
+<table><tr><td>
+<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=211058690808209926622.0004ce68ad4c5d97dafe1&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=42.674343,-83.215277&amp;spn=0.007573,0.013733&amp;z=16&amp;output=embed"></iframe>
+</td>
+<td>
+<strong>Dodge Hall (4 boards):</strong>1 next to room 163, 2 by lower entrance near room 201, 1 across from room 206 </br></br>
+<strong>Hannah Hall (5 boards):</strong>1 by main entrance, 1 next to room 201, 1 next to room 240, 2 next to room 287</br></br>
+<strong>Science and Engineering Building (1 board):</strong>main entrance by the elevator</br></br>
+<strong>Wilson Hall (3 boards):</strong>1 on 2nd floor by the restrooms, 2 next to vending machines on the 1st floor</br></br>
+<strong>Oakland Center (3 boards):</strong>1 next to the book store, 1 next to main level elevator/staricase, 1 at the bottom of the east facing stairwell</br></br>
+<strong>North Foundation Hall (1 board):</strong>across from financial aid</br></br>
+<strong>South Foundation Hall (5 boards):</strong>2 next to the north entrance, 1 by the south entrance, 2 across from the restrooms on the 1st floor</br></br>
+<strong>O'Dowd Hall (4 boards):</strong>1 next to Registrar's Office, 1 across from room 110, 1 next to room 115, 1 across from room 207</br></br>
+<strong>Vandenberg Hall (3 boards):</strong>1 next to reception desk, 1 next to cafeteria, 1 across from honors college</br></br>
+<strong>Varner Hall (9 boards):</strong>1 by the recital hall, 1 by the main entrance from Pawley Hall, 1 next to room 129, 1 next to room 231, 1 next to room 109, 1 next to room 110, 1 next to room 226, 1 next to room 226, 1 next to room 234, 1 across from room 205</br></br>
+<strong>Elliott Hall (1 board):</strong>in the student lounge
+</td>
+</tr>
+<tr>
+<td colspan=2>
+<small>View <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=211058690808209926622.0004ce68ad4c5d97dafe1&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=42.674343,-83.215277&amp;spn=0.007573,0.013733&amp;z=16&amp;source=embed" style="color:#0000FF;text-align:left">OU Campus</a> in a larger map</small>
+</td></tr></table>
+
 </body>
 </html>
