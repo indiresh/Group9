4f49ce430c5c89dfc3a2eb7f730da65bacada424
Ryan D <rmdwyer@oakland.edu>
2012-12-03 02:36:40 -0500

    csa guidelines

---
+++ GrizzBoard/csa_posting_guidelines.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+.style1 {font-family: Arial, Helvetica, sans-serif}
+-->
+</style>
+</head>
+
+<body>
+<h2 align="center">Posting Policy for Oakland University</h2>
+<strong>SUBJECT:</strong> 	      			UNIVERSITY POSTING POLICY </br></br>
+<strong>NUMBER:</strong> 	       			486</br></br>
+<strong>AUTHORIZING BODY:</strong>      	PRESIDENT'S CABINET</br></br>
+<strong>RESPONSIBLE OFFICE:</strong> 		CENTER FOR STUDENT ACTIVITIES</br></br>
+<strong>DATE ISSUED:</strong> 				APRIL 1987</br></br>
+<strong>LAST UPDATE:</strong> 				JANUARY 2002</br></br>
+
+<strong><ins>RATIONALE:</ins></strong></br></br>
+
+<strong><ins>POLICY:</ins></strong></br></br>
+
+ 
+<pre class="style1">
+Posting is defined as the hanging of information, advertising, etc. on any campus bulletin board, window, door, wall or other University property.
+Oakland University reserves the right to refuse permission to any individual or group for posting that
+
+        a.  Would seriously impede normal University activity,
+
+        b.  Does not follow Oakland University guidelines and procedures set forth in this policy,
+
+        c.  Would make posting particular material inappropriate due to special circumstances, and
+
+        d.  Contains contents which are in violation of University policies or ordinances, state or federal law.
+
+<strong>I.  Approval for Posting on Campus</strong>
+
+        A. Recognized Oakland University Organizations
+
+            1.  Any officially recognized Oakland University department or organization may post materials which pertain to the activities of the group
+                 on any authorized bulletin board or display area, subject to restrictions as defined in this policy.
+
+        B. Off-Campus Groups or Individuals
+
+            1.  Any off-campus group or individuals wishing to post any type of materials on campus must obtain prior approval from Center for Student
+                 Activities. If approved, this material may be posted on "Non-CSA Posting Area" boards.
+
+            2.  When advertising for a service, activity or event which is already provided for by a University department or organization or when
+                 materials are not consistent with the University role and mission, a disclaimer must appear on each piece of posted material.  This
+                 disclaimer must state that it is not a "University sponsored" service, activity or event.
+
+<strong>II.  Posting Sites</strong>
+ 
+Generally only bulletin boards are available for use to post information to the campus community.  Groups should ask about the appropriate use of
+bulletin boards prior to use.  "CSA Posting Areas" are reserved for providing information about University activities.  Postings during the annual
+student government elections are subject to Oakland University Student Congress Election Commission elections guidelines.
+ 
+<strong>III.  Posting Regulations</strong>
+
+        1. Posters, flyers and other printed material are not to be affixed to restroom mirrors, plants, trees, poles and painted surfaces in and any
+            OU building, including the Oakland Center and the residence halls.
+
+        2. Only thumbtacks, not staples or tape, are to be used to post printed information.
+
+        3. Printed material posted will be removed monthly.
+
+        4. Any undated material will be removed.
+
+        5. Personal posting or advertisements are not to be posted on bulletin boards designated as "CSA Posting Areas."
+
+        6. Only one posting of printed material can be posted on bulletin boards.
+
+        7. Postings must be within the frame of the bulletin board. Do not post on top of other printed material.  Do not remove other printed material
+            already posted unless outdated. 
+
+        8. There is no posting of adhesive-backed materials (i.e., stickers) on any surfaces, including trees and poles.
+
+        9. Banners can be ordered and scheduled to be hung in the Oakland Center through the Center for Student Activities Office.
+
+       10. Banners or other printed materials to be hung or posted in and around the residence halls must be approved through the Housing Office.
+
+       11. Placement of table tents in the Pioneer Food Court can be scheduled through the Center for Student Activities Office, and Vandenberg Dining
+             Center table tents through the Housing Office. 
+
+       12. Posting by "chalking" can be done on sidewalks where there is no overhang and chalk can be washed away by inclement weather. Chalking
+             cannot be done on any surface except sidewalks and the bridge. Paint of any kind is prohibited, including chalk paint. 
+
+       13. Paint can be used only to decorate the "spirit rocks" located outside of the Oakland Center (north end) and in the Hamlin Circle.
+
+       14. There is no posting on vehicles in any University parking lot.
+
+       15. Student and Greek organization mailboxes in the Center for Student Activities Office can be used for distributing information about on-or
+             off-campus activities, products and services with approval from the CSA staff.
+
+       16. Mailboxes in the residence halls are not available for distributing information about on-or off-campus activities sponsored by student and
+             Greek organizations.
+
+Violating the posting policy can result in disciplinary action and/or service charges for individuals and groups found responsible.  Violators may also
+be charged with violation of OU ordinance #4.21.
+ 
+<strong>IV.  Disclaimer</strong>
+
+By its review and acceptance of materials for posting on campus, Oakland University does not endorse or warrant the products and services advertised on
+these printed materials.
+ 
+<strong>V.  Enforcement</strong>
+
+Dean of Students Office
+Center for Student Activities
+ 
+Additional information about the posting policy or complaints regarding violations of this policy can be directed to the Dean of Students Office or the
+Center for Student Activities. 
+</pre>
+</body>
+</html>
