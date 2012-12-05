6a88b7179eefb9c4184ae766d28152177c4a0a01
Seth Jones <seth.jones@nerdheroes.com>
2012-12-05 12:11:21 -0500

    Add listing table.

---
+++ GrizzBoard/main.php
@@ -0,0 +1,4 @@
+<h1>Welcome to the GRIZZBoard!</h1>
+<p><img src="images/University-Bulletin-Board.jpg" width="500" height="391" align="right"></p>
+<p>What is the GRIZZBoard:</p>
+<p>How to use the GRIZZBoard:</p>
---
+++ GrizzBoard/view-postings.php
@@ -0,0 +1,41 @@
+<?php 
+include "mysql-connector.php";
+$query="SELECT * FROM listing_info";
+$result=mysql_query($query);
+
+$num=mysql_numrows($result);
+
+mysql_close();
+?>
+<table border="0" cellspacing="2" cellpadding="2">
+<tr>
+<td><font face="Arial, Helvetica, sans-serif"><b>Listing ID</b></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><b>Start Date</b></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><b>End Date</b></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><b>Listing User</b></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><b>Content</b></font></td>
+</tr>
+
+<?php
+$i=0;
+while ($i < $num) {
+
+$f1=mysql_result($result,$i,"id");
+$f2=mysql_result($result,$i,"startDate");
+$f3=mysql_result($result,$i,"endDate");
+$f4=mysql_result($result,$i,"user");
+$f5=mysql_result($result,$i,"content");
+?>
+
+<tr>
+<td><font face="Arial, Helvetica, sans-serif"><?php echo $f1; ?></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><?php echo $f2; ?></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><?php echo $f3; ?></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><?php echo $f4; ?></font></td>
+<td><font face="Arial, Helvetica, sans-serif"><?php echo $f5; ?></font></td>
+</tr>
+
+<?php
+$i++;
+}
+?>
