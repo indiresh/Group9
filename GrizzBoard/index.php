b9099e8cf7fd1a9613699eb24029dba01c57b2a6
Seth Jones <seth.jones@nerdheroes.com>
2012-12-05 16:29:51 -0500

    Final icon link

--- GrizzBoard/index.php
+++ GrizzBoard/index.php
@@ -31,14 +31,14 @@
     </div>
     </a>
     <div class="small-link"><a href="index.php?page=view-postings.php">
-      <div class="small-icon"><img src="images/postings-icon.png" width="128" height="128" /></div>
+      <div class="small-icon"><img src="images/postings-icon.png" width="125" height="125" /></div>
       </a> </div>
-    <div class="small-link">
-      <div class="small-icon"></div>
-      Campus Map </div>
-    <div class="small-link">
-      <div class="small-icon"></div>
-    </div>
+   <div class="small-link"><a href="index.php?page=map.php">
+      <div class="small-icon"><img src="images/map-icon.png" width="125" height="125" /></div>
+      </a> </div>
+       <div class="small-link"><a href="index.php?page=csa_posting_guidelines.php">
+      <div class="small-icon"><img src="images/guidelines-icon.png" width="125" height="125" /></div>
+      </a> </div>
   </div>
 </div>
 <div class="footer-wrapper">
